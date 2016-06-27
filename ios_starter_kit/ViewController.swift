352cd78f6929b6b5e0ac6209ee96acede206a238
starhoshi <kensuke1751@gmail.com>
2016-06-27 15:25:27 +0900

    init commit.

---
+++ ios_starter_kit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_starter_kit
+//
+//  Created by 星川　健介 on 2016/06/27.
+//  Copyright © 2016年 star__hoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
